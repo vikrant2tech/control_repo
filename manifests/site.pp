18485b8396c15c8463361a4c392487e3e21e762d
vikrant2tech <vikrant2tech@gmail.com>
2019-02-11 03:08:47 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,7 +9,8 @@ node 'master.puppet.vm' {
   include role::master_server
 }
 node 'web.puppet.vm' {
+  include role::app_server
 }
 node 'db.puppet.vm' {
-  include role::master_server
+  include role::db_server
 }
